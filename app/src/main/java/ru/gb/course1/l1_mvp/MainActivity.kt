5d8f48b978ea3a1143161ec18b746edf7eb88886
Kirill Shalnov <borhammere@gmail.com>
2022-01-21 20:39:56 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/course1/l1_mvp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gb.course1.l1_mvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
