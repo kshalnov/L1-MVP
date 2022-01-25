6e4372e07426c88e0400086df525b5119c28a44a
Kirill Shalnov <borhammere@gmail.com>
2022-01-25 21:47:26 +0300

    Mock pseudo-MVI

--- app/src/main/java/ru/gb/course1/l1_mvp/EmployeesListContract.kt
+++ app/src/main/java/ru/gb/course1/l1_mvp/EmployeesListContract.kt
@@ -1,21 +1,15 @@
 package ru.gb.course1.l1_mvp
 
 import android.content.Context
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 // это должно лежать где-то в domain/entity пакете
 data class EmployeeEntity(
     val name: String,
     val salary: Int
 )
-
-/**
- *      x`   1) Возможная утечка view (activity, fragment, context)
- *      x    2) Восстановление состояния view при detach/attach
- *      3) Одноразовые события
- */
 
 data class OneTimeError(
     private val error: Throwable
@@ -29,41 +23,86 @@ data class OneTimeError(
     }
 }
 
+(R)(Y)(G) = 8
+
+// Хорошие состояния
+(*)()()
+()(*)()
+()()(*)
+()()()
+
+// Плохие
+(*)(*)(*)
+(*)(*)()
+()(*)(*)
+(*)()(*)
+
 interface EmployeesListContract {
+    // MVI - Model View Intent
+    sealed interface MviViewState {
+        class IdleState: MviViewState
+        data class LoadingState(val progress: Int): MviViewState
+        data class ContentState(val employeeList: List<EmployeeEntity>): MviViewState
+        data class ErrorState(val error: Throwable): MviViewState
+    }
 
-    interface ViewModel {
-        val progress: LiveData<Boolean>
-        val employeesList: LiveData<List<EmployeeEntity>?>
-        val error: LiveData<Throwable>
+    sealed class MviAction {
+        data class Error(
+            val msg: String,
+            val thr: Throwable
+        )
 
-        fun onLoadList()
+        data class LicenseAlert(val html: String)
+    }
+
+    sealed interface MviEvent {
+        data class ShowEmployee(val employeeEntity: EmployeeEntity) : MviEvent
+        class LoadList : MviEvent
+    }
+
+    interface MviViewModel {
+        val viewState: LiveData<MviViewState>
+        val actions: LiveData<MviAction> // todo завернуть в SingleEventLiveData
+
+        fun onEvent(event: MviEvent)
     }
 }
 
-class ViewModel : EmployeesListContract.ViewModel {
+class ViewModel : EmployeesListContract.MviViewModel {
+    private var lastViewState: EmployeesListContract.MviViewState = EmployeesListContract.MviViewState.IdleState()
+
     private fun <T> LiveData<T>.mutable(): MutableLiveData<T> {
         return this as MutableLiveData<T>
     }
 
-    override val progress: LiveData<Boolean> = MutableLiveData<Boolean>()
+    override val viewState: LiveData<EmployeesListContract.MviViewState> = MutableLiveData()
 
-    private val _employeesList = MutableLiveData<List<EmployeeEntity>?>()
-    override val employeesList = _employeesList as LiveData<List<EmployeeEntity>?>
+    override val actions: LiveData<EmployeesListContract.MviAction>
+        get() = TODO("Not yet implemented")
 
-    private val _error = MutableLiveData<Throwable>()
-    override val error = _error as LiveData<Throwable>
 
-    override fun onLoadList() {
-        progress.mutable().postValue(true)
+    override fun onEvent(event: EmployeesListContract.MviEvent) {
+        when (event) {
+            is EmployeesListContract.MviEvent.LoadList -> {
+                onLoadList()
+            }
+            is EmployeesListContract.MviEvent.ShowEmployee -> {
+                // todo
+            }
+        }
+    }
 
+    private fun onLoadList() {
+        lastViewState = EmployeesListContract.MviViewState.LoadingState(0)
+        viewState.mutable().postValue(lastViewState)
         api.loadUsers( // 30 sec
             onError = { error: Throwable ->
-                _progress.postValue(false)
-                _error.postValue(error)
+                lastViewState = EmployeesListContract.MviViewState.ErrorState(error)
+                viewState.mutable().postValue(lastViewState)
             },
             onSuccess = { data: List<EmployeeEntity> ->
-                _progress.postValue(false)
-                _employeesList.postValue(data)
+                lastViewState = EmployeesListContract.MviViewState.ContentState(data)
+                viewState.mutable().postValue(lastViewState)
             }
         )
     }
@@ -73,19 +112,53 @@ class ViewModel : EmployeesListContract.ViewModel {
 
 class EmployeesView {
     private lateinit var context: Context
-    private lateinit var viewModel: EmployeesListContract.ViewModel
+    private lateinit var viewModel: EmployeesListContract.MviViewModel
 
     fun onCreate() {
         // ...
+        viewModel.onEvent(EmployeesListContract.MviEvent.LoadList())
 
-        viewModel.progress.observe(this) {
-            binding.progressBar.isVisible = it
+        button.setOnClickListener { v ->
+            viewModel.onEvent(EmployeesListContract.MviEvent.ShowEmployee())
         }
 
-        viewModel.error.observe(this) {
-            Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
-        }
-
+        viewModel.viewState.observe(this) { state -> render(state) }
     }
+
+    private fun render(state: EmployeesListContract.MviViewState) {
+        when (state) {
+            is EmployeesListContract.MviViewState.ContentState -> renderContent(state)
+            is EmployeesListContract.MviViewState.ErrorState -> renderError(state)
+            is EmployeesListContract.MviViewState.IdleState -> renderIdle(state)
+            is EmployeesListContract.MviViewState.LoadingState -> renderLoading(state)
+        }
+    }
+
+    private fun renderLoading(state: EmployeesListContract.MviViewState.LoadingState) {
+        errorView.isVisible = false
+
+        progressView.isVisible = state.progress
+    }
+
+    private fun renderIdle(state: EmployeesListContract.MviViewState.IdleState) {
+        progressView.isVisible = false
+        errorView.isVisible = false
+        // ничего
+    }
+
+    private fun renderError(state: EmployeesListContract.MviViewState.ErrorState) {
+        progressView.isVisible = false
+
+        errorView.isVisible = state.error
+        errorTextView.text = state.error.message
+    }
+
+    private fun renderContent(state: EmployeesListContract.MviViewState.ContentState) {
+        progressView.isVisible = false
+        errorView.isVisible = false
+
+        adapter.setData(state.employeeList)
+    }
+
 
 }
